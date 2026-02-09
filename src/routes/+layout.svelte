8a56ac8bfc1abcdac2cd78687e60e06c6601b01c
iyan subdiana <iyanonimous@pop-os.ostrich-komodo.ts.net>
2026-02-09 10:07:42 +0700

    initial push project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import favicon from "$lib/assets/favicon.svg";
+	import "bootstrap-icons/font/bootstrap-icons.css";
+	import "@tabler/core/dist/css/tabler.min.css";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<main class="container-fluid">
+	{@render children()}
+</main>
